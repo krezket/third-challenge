cdd6034ce33d9b313815d88a1843b5b1239d2576
Anthony <anthonyguerrero62@outlook.com>
2023-03-30 14:06:31 -0700

    added semicolons haha

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,12 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  alert("Your new password has been generated.")
-  console.log(password)
+  alert("Your new password has been generated.");
+  console.log(password);
   var passwordText = document.querySelector("#password");
-  capOne = 1
-  numOne = 1
-  spclOne = 1
+  capOne = 1;
+  numOne = 1;
+  spclOne = 1;
 
   passwordText.value = password;
 }
@@ -22,11 +22,11 @@ let choises =
 'abcdefghijklmnopqrstuvwxyz', 
 'abcdefghijklmnopqrstuvwxyz!!@@##$$%%&&??', 
 'abcdefghijklmnopqrstuvwxyz0123456789', 
-'abcdefghijklmnopqrstuvwxyz0123456789!!@@##$$%%&&??'] 
+'abcdefghijklmnopqrstuvwxyz0123456789!!@@##$$%%&&??']; 
 
-let capOne = 1
-let numOne = 1
-let spclOne = 1
+let capOne = 1;
+let numOne = 1;
+let spclOne = 1;
 
 function countCapTrue() {
   capOne ++;
@@ -55,47 +55,47 @@ function generatePassword() {
      cap();
   }
   else if (inputLength > 128) {
-    alert("Bruh....... Too many characters.")
+    alert("Bruh....... Too many characters.");
   }
   else if (inputLength < 8) {
-    alert("Bruh. Not enough characters.")
+    alert("Bruh. Not enough characters.");
   }
   else {
-    alert("Error: \nNumbers only! \n(8-128)")
+    alert("Error: \nNumbers only! \n(8-128)");
   }
 
   function cap() {
     var capLet = confirm("Would you like to add capital letters? \n(Recommended)");
     console.log(capLet);
     if (capLet === true) {
-      countCapTrue()
+      countCapTrue();
     }
     else {
-      lessCapFalse()
+      lessCapFalse();
     }
-    num()
+    num();
   }
 
   function num() {
     var numB = confirm("Would you like to add numbers? \n(Recommended)");
     console.log(numB);
     if (numB === true) {
-      countNumTrue()
+      countNumTrue();
     }
     else {
-      lessNumFalse()
+      lessNumFalse();
     }
-    spcl()
+    spcl();
   }
 
   function spcl() {
     var spclChar = confirm("Would you like to add special characters? \n(Recommended)");
     console.log(spclChar);
     if (spclChar === true) {
-      countSpclTrue()
+      countSpclTrue();
     }
     else {
-      lessSpclFalse()
+      lessSpclFalse();
     }
   }
   
@@ -109,100 +109,100 @@ function generatePassword() {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
   else if ((capOne === 2) && (numOne === 2) && (spclOne === 0)) {
     function pass(inputLength) {
       let result ='';
       const characters = choises[1];
-      const charactersLength = characters.length
+      const charactersLength = characters.length;
       let counter = 0;
       while (counter < inputLength) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
   else if ((capOne === 2) && (numOne === 0) && (spclOne === 2)) {
     function pass(inputLength) {
       let result ='';
       const characters = choises[2];
-      const charactersLength = characters.length
+      const charactersLength = characters.length;
       let counter = 0;
       while (counter < inputLength) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
   else if ((capOne === 2) && (numOne === 0) && (spclOne === 0)) {
     function pass(inputLength) {
       let result ='';
       const characters = choises[3];
-      const charactersLength = characters.length
+      const charactersLength = characters.length;
       let counter = 0;
       while (counter < inputLength) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
   else if ((capOne === 0) && (numOne === 0) && (spclOne === 0)) {
     function pass(inputLength) {
       let result ='';
       const characters = choises[4];
-      const charactersLength = characters.length
+      const charactersLength = characters.length;
       let counter = 0;
       while (counter < inputLength) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
   else if ((capOne === 0) && (numOne === 0) && (spclOne === 2)) {
     function pass(inputLength) {
       let result ='';
       const characters = choises[5];
-      const charactersLength = characters.length
+      const charactersLength = characters.length;
       let counter = 0;
       while (counter < inputLength) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
   else if ((capOne === 0) && (numOne === 2) && (spclOne === 0)) {
     function pass(inputLength) {
       let result ='';
       const characters = choises[6];
-      const charactersLength = characters.length
+      const charactersLength = characters.length;
       let counter = 0;
       while (counter < inputLength) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
   else if ((capOne === 0) && (numOne === 2) && (spclOne === 2)) {
     function pass(inputLength) {
       let result ='';
       const characters = choises[7];
-      const charactersLength = characters.length
+      const charactersLength = characters.length;
       let counter = 0;
       while (counter < inputLength) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         counter += 1;
       }
-      return result
+      return result;
    }
   }
-  return (pass(inputLength))
+  return (pass(inputLength));
 }
 generateBtn.addEventListener("click", writePassword);
