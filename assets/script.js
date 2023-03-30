94b2eadccaeab13141d1e9603522b38ef18cf528
Anthony <anthonyguerrero62@outlook.com>
2023-03-30 14:02:32 -0700

    removed unnecessary code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
